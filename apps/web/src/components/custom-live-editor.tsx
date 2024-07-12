7fe4cc5de15ba62fed540411122b76d670bf2c7d
jongmyeong <polcaneli@gmail.com>
2024-07-12 23:48:52 +0900

    fix: 기타 수정

--- apps/web/src/components/custom-live-editor.tsx
+++ apps/web/src/components/custom-live-editor.tsx
@@ -5,7 +5,7 @@ import { Scaffold, ScaffoldBody, ScaffoldHeader } from 'layouts-rc';
 export function CustomLiveEditor() {
   return (
     <Scaffold className="flex-1 max-w-[700px]">
-      <ScaffoldHeader className="text-lg flex items-center gap-2 bg-white rounded-t-md px-4 font-semibold">
+      <ScaffoldHeader className="text-lg flex items-center gap-2 bg-white rounded-t-md px-4 font-semibold text-black">
         <span>Live code</span>
         <div className="bg-red-500 rounded-full w-3 h-3 animate-pulse" />
       </ScaffoldHeader>
